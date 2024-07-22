1e12bc3a5f1e9e09d2555f871906b549456f575c
wilmadsy <ahmad.ayyasy.08@gmail.com>
2024-07-22 10:27:26 +0700

    update

---
+++ constanta.php
@@ -0,0 +1,21 @@
+<?php
+// membuat konstanta dengan fungsi define()
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'petanikode');
+define('DB_PASS', 'R4Hasia');
+define('DB_NAME', 'belajar');
+
+// membuat konstanta dengan kata kunci const
+const API_KEY = "1234";
+?>
+
+<?php
+
+define('VERSION', '1.0.0');
+
+const SITE_NAME = "petani kode";
+const BASE_URL = "https:// www.petani kode.com";
+
+echo "Site name: " . SITE_NAME . "<br/>";
+echo "URL: " . BASE_URL . "<br/>";
+echo "Version: " . VERSION . "<br/>";
